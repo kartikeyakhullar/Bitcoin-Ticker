4a6ddbb3957a47c56ba40e7c655236c4fbf72785
Kartikeya <kartikeya.knightriders@gmail.com>
2020-02-01 00:26:09 +0530

    Added amount parameter.

--- index.js
+++ index.js
@@ -20,22 +20,28 @@ app.post("/", function(req,res){
 
     var crypto = req.body.crypto;
     var fiat = req.body.fiat;
+    var amount = req.body.amount;
 
-    var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
-    var finalURL = baseURL + crypto + fiat;
+    var options = {
+        url : "https://apiv2.bitcoinaverage.com/convert/global",
+        method : "GET",
+        qs : {
+            from : crypto,
+            to : fiat,
+            amount : amount
+        }
 
-    request(finalURL, function(error,res,body){
+    }
+
+
+    request(options, function(error,res,body){
         var data = JSON.parse(body);
-        var price = data.last;
-        var currentDate = data.display_timestamp;
-        res.send("<h1>The current price of " + crypto + " is " + price + fiat + "</h1>");
+        var price = data.price;
+        var currentDate = data.time;
+        res.send("<h1>" + amount + " " + crypto + " is worth " + price + " " + fiat + "</h1>");
     })
 
 })
-
-
-
-
 
 
 app.listen(3000, function(){
